7c76df1bf8e09e64c67d4e0914ba682ca54f76f9
freak <artback@protonmail.com>
2021-06-24 12:01:19 +0200

    Add mongodb

--- cmd/pds/main.go
+++ cmd/pds/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"log"
 	"micro_services/api/v1/port"
 	"micro_services/pkg/pds"
+	"micro_services/pkg/pds/mongodb"
 	"net"
 )
 
@@ -15,8 +20,19 @@ func main() {
 		panic(err)
 	}
 	srv := grpc.NewServer()
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
-	port.RegisterPDServiceServer(srv, &pds.PortDomainService{})
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	port.RegisterPDServiceServer(srv, &pds.PortDomainService{mongodb.PortRepository{client}})
 
 	reflection.Register(srv)
 
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/golang/protobuf v1.5.2
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	go.mongodb.org/mongo-driver v1.5.3
 	google.golang.org/genproto v0.0.0-20210617175327-b9e0b3197ced
 	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.26.0
